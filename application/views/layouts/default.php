48fbb0a1c0a971acbecde9689515b7b2fe5878f7
puterakahfi <puterakahfi@gmail.com>
2014-08-16 05:46:46 +0700

    added template

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,40 +1,27 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?php echo $template['title']; ?></title>
-		<?php echo $template['metadata']; ?>
-	</head>
-	<body>
-		<h1><?php echo $template['title']; ?></h1>
-		<?php echo $template['body']; ?>
-	</body>
-</html>
-
-
-<!DOCTYPE html>
-<html>
     <head>
         <meta charset="UTF-8">
         <title>AdminLTE | Dashboard</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- bootstrap 3.0.2 -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
         <!-- font Awesome -->
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/font-awesome.min.css');?>" rel="stylesheet" type="text/css" />
         <!-- Ionicons -->
-        <link href="css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/ionicons.min.css');?>" rel="stylesheet" type="text/css" />
         <!-- Morris chart -->
-        <link href="css/morris/morris.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/morris/morris.css');?>" rel="stylesheet" type="text/css" />
         <!-- jvectormap -->
-        <link href="css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/jvectormap/jquery-jvectormap-1.2.2.css');?>" rel="stylesheet" type="text/css" />
         <!-- Date Picker -->
-        <link href="css/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/datepicker/datepicker3.css');?>" rel="stylesheet" type="text/css" />
         <!-- Daterange picker -->
-        <link href="css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/daterangepicker/daterangepicker-bs3.css');?>" rel="stylesheet" type="text/css" />
         <!-- bootstrap wysihtml5 - text editor -->
-        <link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css');?>" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
-        <link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/lte/css/AdminLTE.cs');?>s" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -883,36 +870,36 @@
         <!-- jQuery 2.0.2 -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
         <!-- jQuery UI 1.10.3 -->
-        <script src="js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/jquery-ui-1.10.3.min.js');?>" type="text/javascript"></script>
         <!-- Bootstrap -->
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/bootstrap.min.js');?>" type="text/javascript"></script>
         <!-- Morris.js charts -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="js/plugins/morris/morris.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/morris/morris.min.js');?>" type="text/javascript"></script>
         <!-- Sparkline -->
-        <script src="js/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/sparkline/jquery.sparkline.min.js');?>" type="text/javascript"></script>
         <!-- jvectormap -->
-        <script src="js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
-        <script src="js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js');?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js');?>" type="text/javascript"></script>
         <!-- jQuery Knob Chart -->
-        <script src="js/plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/jqueryKnob/jquery.knob.js');?>" type="text/javascript"></script>
         <!-- daterangepicker -->
-        <script src="js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/daterangepicker/daterangepicker.js');?>" type="text/javascript"></script>
         <!-- datepicker -->
-        <script src="js/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/datepicker/bootstrap-datepicker.js');?>" type="text/javascript"></script>
         <!-- Bootstrap WYSIHTML5 -->
-        <script src="js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js');?>" type="text/javascript"></script>
         <!-- iCheck -->
-        <script src="js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/plugins/iCheck/icheck.min.js" type="text/javascript');?>"></script>
 
         <!-- AdminLTE App -->
-        <script src="js/AdminLTE/app.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/AdminLTE/app.js');?>" type="text/javascript"></script>
 
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="js/AdminLTE/dashboard.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/AdminLTE/dashboard.js');?>" type="text/javascript"></script>
 
         <!-- AdminLTE for demo purposes -->
-        <script src="js/AdminLTE/demo.js" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/lte/js/AdminLTE/demo.js');?>" type="text/javascript"></script>
 
     </body>
 </html>
